866bd44e76fc404fdbc4d3b8b8bd64cfdeb8de8b
Martin <94791875+PomeloPack@users.noreply.github.com>
2023-06-27 11:33:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ width = 1024
 height = 768
 screen = pygame.display.set_mode((width, height))
 
+# main game cycle
+game_continue = True
+    
+while game_continue:
+    for event in pygame.event.get(): # every move or mouse/key click or any other stuff is called event in pygame
+        if event.type == pygame.QUIT:
+            game_continue = False
 
 
 
