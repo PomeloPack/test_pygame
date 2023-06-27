154e2e73a8051bee1fff1b087063266e1a8f2565
Martin <94791875+PomeloPack@users.noreply.github.com>
2023-06-27 11:22:26 +0200

    test

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+# initialize pygame - start
+pygame.init()
+
+# screen create
+width = 1024
+height = 768
+screen = pygame.display.set_mode((width, height))
+
+
+
+
+
+
+# end of initialization
+pygame.quit()
